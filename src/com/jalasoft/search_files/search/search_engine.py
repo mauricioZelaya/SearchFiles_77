ebeb4a549ea6d00d2c9d3604f8357dfc02e7324d
Mauricio Zelaya <zelaya.mauricio@gmail.com>
2018-01-23 17:54:22 -0400

    listing directories for a given path, create unit test file

--- src/com/jalasoft/search_files/search/search_engine.py
+++ src/com/jalasoft/search_files/search/search_engine.py
@@ -1,16 +1,19 @@
 import os
 
-# for root, dirs, files in os.walk("C:\Users\Administrator\Documents\DevFund2\SearchFiles_77"):
-#    for name in files:
-#       print(os.path.join(root, name))
-   # for name in dirs:
-   #    print(os.path.join(root, name))
 
-print("root prints out directories only from what you specified")
-print("dirs prints out sub-directories from root")
-print("files prints out all files from root and directories")
-print("*" * 20)
-for root, dirs, files in os.walk("/"):
-    print(root)
-    print(dirs)
-    print(files)
+def print_directory(path):
+    return os.listdir(path)
+
+# def search_file(path, file_name):
+#     for root, dirs, files in os.walk(path):
+#         print(root)
+#         # if file_name in files:
+#         #     print("file path directory: %s" % root)
+#         #     print(dirs)
+#         #     print(files)
+#         #     print("exist!!!")
+
+
+# search_file("D:", "Behave_1.pptx")
+directory_list = print_directory("D:\MauricioZ\\")
+print(directory_list)
---
+++ test/com/jalasoft/search_files/search/search_engine_tests.py
@@ -0,0 +1,3 @@
+from src.com.jalasoft.search_files_search.search_engine import *
+
+def verify_that_given_a_path_
