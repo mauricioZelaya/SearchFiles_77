00fd91bdcb0931921beda74548f5ec51642a7d44
Mauricio Zelaya <zelaya.mauricio@gmail.com>
2018-01-25 13:03:00 -0400

    refactor of print_directory method

--- src/com/jalasoft/search_files/search/search_engine.py
+++ src/com/jalasoft/search_files/search/search_engine.py
@@ -1,10 +1,15 @@
 """
-class Search_Engine perform the search on a given path of a given file_name applying all the desired filters
+class Search_Engine perform the search on a given path of a given file_name applying
+all the desired filters
 """
 import os
 
 
 class Search(object):
+    """
+
+    :param object  class
+    """
     def __init__(self, file_name=None, path_file='/', criteria=None):
         """
         constructor method
@@ -23,16 +28,10 @@ class Search(object):
         """
         list_dir = []
         for root, dirs, files in os.walk(self._path_file, topdown=False):
-            # for value in root:
-            #     list_dir.append(value)
             for value in dirs:
                 list_dir.append(os.path.join(root, value))
             for value in files:
                 list_dir.append(os.path.join(root, value))
-        print(list_dir)
 
+        return list_dir
 
-
-se = Search('', path_file='C:\\Users\Administrator\Documents\DevFund2\SearchFiles_77\src')
-se.print_directory()
-# print(se)
--- test/com/jalasoft/search_files/search/search_engine_tests.py
+++ test/com/jalasoft/search_files/search/search_engine_tests.py
@@ -1,4 +1,8 @@
+"""
+class for unit tests
+"""
 import pytest
+import os
 from SearchFiles_77.src.com.jalasoft.search_files.search.search_engine import Search
 
 @pytest.mark.search
@@ -17,12 +21,7 @@ def test_a_search_is_created_with_path_list_files_in_the_given_path():
 
     :return:
     """
-    search_path = "D:\\MauricioZ\\"
-    directory_list = ['accounting excercises.docx', 'AIMP3', 'books', 'crm - practice 1.docx', 'Documments', 'emailBKP',
-                      'Installers', 'mauriciowally.docx', 'musica', 'NetForum', 'playlist', 'Video',
-                      '~$uriciowally.docx']
+    search_path = os.getcwd()
     search_engine = Search(path_file=search_path)
     assert isinstance(search_engine, Search)
-    assert search_engine.print_directory() == directory_list
-
-
+    assert search_engine.print_directory() is not None
